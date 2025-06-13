c1d729abf262c9725882ca6b06e1832e1ffb2274
awwwdde <sjuu1337@gmail.com>
2025-06-14 00:11:02 +0300

    0.2 // start dev

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+    Экосистема AI сотрудников <br/>Автоматизируйте аналитические и ручные процессы вашего бизнеса с помощью Ударников.
+    </div>
+  );
+};
+
+export default MainPage;
